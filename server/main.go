ac4938c33e4d38fc0734864a969c5d8fd7effbee
Ian Wood <ianwood103@gmail.com>
2024-05-04 23:11:08 -0400

    add postgres db config

--- server/main.go
+++ server/main.go
@@ -13,6 +13,10 @@ func main() {
 
 	routes := httprouter.New()
 
+	routes.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Fprint(w, "Welcome Home")
+	})
+
 	server := http.Server{Addr: "localhost:8888", Handler: routes}
 
 	err := server.ListenAndServe()
