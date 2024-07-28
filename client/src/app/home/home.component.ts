5dd971aab913fd1b71b702616755cde605e835db
Ian Wood <ianwood103@gmail.com>
2024-07-28 16:11:00 -0700

    iterate through notes

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,22 +1,27 @@
 import { Component } from '@angular/core';
+import { NgIf, NgFor } from '@angular/common';
 import { NoteService } from '../services/note.service';
-import { NotesResponse } from '../../types';
+import { Note, NotesResponse } from '../../types';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  notes: Note[] | null = null;
+
   constructor(private noteService: NoteService) {}
 
   ngOnInit() {
     this.noteService
       .getNotes('http://localhost:8888/api/note')
-      .subscribe((notes: NotesResponse) => {
-        console.log(notes);
+      .subscribe((response: NotesResponse) => {
+        if (response.code == 200) {
+          this.notes = response.data;
+        }
       });
   }
 }
